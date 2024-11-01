7661d295650ae8ae328e50b840f2ff0d76ddfb55
GabeLeme <gabioliveira_leme@outlook.com>
2024-11-01 13:46:13 -0300

    Atualização

--- AC2/js/script.js
+++ AC2/js/script.js
@@ -1,14 +1,20 @@
-const dropdownButton = document.getElementById('user-dropdown');
-    const dropdownMenu = document.getElementById('user-options');
+// Obtém o botão do dropdown
+// dropdown: menu suspenso
+const dropdownButton = document.getElementById('user-dropdown'); // Botão que ativa
+const dropdownMenu = document.getElementById('user-options'); // Menu que será mostrado ou escondido
 
-    dropdownButton.addEventListener('click', () => {
-        const isVisible = dropdownMenu.style.display === 'block';
-        dropdownMenu.style.display = isVisible ? 'none' : 'block';
-    });
+// Adiciona um evento de clique no botão do dropdown
+dropdownButton.addEventListener('click', () => {
+    // Verifica se o menu está visível
+    const isVisible = dropdownMenu.style.display === 'block'; // se estiver block o menu vai estar visivel
+    // Alterna a visibilidade do menu
+    dropdownMenu.style.display = isVisible ? 'none' : 'block'; // Se visível, oculta; caso contrário, exibe
+});
 
-    // Para fechar o menu se clicar fora dele
-    window.addEventListener('click', (event) => {
-        if (!dropdownButton.contains(event.target) && !dropdownMenu.contains(event.target)) {
-            dropdownMenu.style.display = 'none';
-        }
-    });
+// Para fechar o menu se clicar fora dele
+window.addEventListener('click', (event) => {
+    //verifica se o clique foi dentro ou fora 
+    if (!dropdownButton.contains(event.target) && !dropdownMenu.contains(event.target)) {
+        dropdownMenu.style.display = 'none'; //oculta o menu se clicar fora
+    }
+});
