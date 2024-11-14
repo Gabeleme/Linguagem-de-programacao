9607455c19d6b6b3f8b8c6b2fdd049c4ce22f668
GabeLeme <gabioliveira_leme@outlook.com>
2024-11-14 13:14:29 -0300

    Historia - Java e JavaScript

---
+++ História - Java e JavaScript/Java/src/ex05_Execucao_Assincrona.java
@@ -0,0 +1,16 @@
+
+// Execução Assíncrona
+// Java (Threads)
+// Java lida com tarefas assíncronas principalmente através de threads.
+
+public class ex05_Execucao_Assincrona extends Thread {
+    @Override
+    public void run() {
+        System.out.println("Tarefa em segundo plano em Java!");
+    }
+
+    public static void main(String[] args) {
+        ex05_Execucao_Assincrona tarefa = new ex05_Execucao_Assincrona();
+        tarefa.start();
+    }
+}
